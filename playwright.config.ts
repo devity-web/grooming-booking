c65fd6b69a4c7c0e17639b97b9f43fa262a44263
Matheus Pedroni <pedroni.dev@gmail.com>
2026-08-13 16:32:53 +0100

    feat: fix lint

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,12 +6,7 @@ export default defineConfig({
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI
-    ? [
-        ['github'],
-        ['html', {open: 'never'}],
-      ]
-    : 'html',
+  reporter: process.env.CI ? [['github'], ['html', {open: 'never'}]] : 'html',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
